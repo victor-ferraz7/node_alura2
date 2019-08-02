437e2806038eddf6ea4fb13246ce56989e97ae4e
Victor Ferraz <victor.fzn7@gmail.com>
2019-08-02 15:44:44 -0300

    Refatoração das Rotas

---
+++ src/app/rotas/livros-rotas.js
@@ -0,0 +1,22 @@
+const LivroControlador = require('../controladores/livro-controlador');
+const livroControlador = new LivroControlador();
+
+const Livro = require('../modelos/livro');
+
+const { check } = require('express-validator/check');
+
+module.exports = (app) => {
+   
+    const rotasLivro = LivroControlador.rotas();
+    
+    app.get(rotasLivro.lista, livroControlador.lista());
+
+    app.route(rotasLivro.cadastro)
+            .get(livroControlador.formularioCadastro())
+            .post(Livro.validacoes(), livroControlador.cadastra())
+            .put(livroControlador.edita());
+
+    app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
+
+    app.delete(rotasLivro.delecao, livroControlador.remove());
+};
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,28 +1,7 @@
-const LivroControlador = require('../controladores/livro-controlador');
-const livroControlador = new LivroControlador();
-
-const BaseControlador = require('../controladores/base-controlador');
-const baseControlador = new BaseControlador();
-
-const Livro = require('../modelos/livro');
-
-const { check } = require('express-validator/check');
+const livroRotas = require('./livros-rotas');
+const baseRotas = require('./base-rotas');
 
 module.exports = (app) => {
-    const rotasBase = BaseControlador.rotas();
-    const rotasLivro = LivroControlador.rotas();
-
-    app.get(rotasBase.home, baseControlador.home());
-    
-    app.get(rotasLivro.lista, livroControlador.lista());
-
-    app.get(rotasLivro.cadastro, livroControlador.formularioCadastro());
-
-    app.get(rotasLivro.edicao, livroControlador.formularioEdicao());
-
-    app.post(rotasLivro.lista, Livro.validacoes(), livroControlador.cadastra());
-
-    app.put(rotasLivro.lista, livroControlador.edita());
-
-    app.delete(rotasLivro.delecao, livroControlador.remove());
+    baseRotas(app);
+    livroRotas(app);
 };
